64057539249c3b15aa293ae17f64c5b4f3772d65
shadow0419 <smartfinance0419@gmail.com>
2021-11-19 23:52:04 +0900

    fix: tokenomics description fix

--- src/component/custom/Landing/Tokenomics.js
+++ src/component/custom/Landing/Tokenomics.js
@@ -10,11 +10,14 @@ import token from "../../../assets/token.png";
 import car1 from "../../../assets/car1.png";
 import car2 from "../../../assets/car2.png";
 import { Col } from "../../common/Layout";
+import { Text } from "../../common/Text";
 
 const TokenomicsDiv = styled.div`
   position: relative;
+
   background-color: rgb(28, 20, 43);
   background-image: url(${token});
+  backdrop-filter: blur(10px);
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
@@ -42,44 +45,39 @@ const RoadmapText = styled.div`
   text-align: center;
   margin-top: 137px;
 `;
-
-const TokenItems = styled.div`
+const StyledToken = styled.div`
   background-image: url(${token_back});
   background-repeat: no-repeat;
   background-size: cover;
   border-radius: 50px;
-  width: 90%;
-  margin-bottom: 137px;
-  margin-top: 52px;
+  width: 80%;
+`;
+const TokenItems = styled.div`
+  display: grid;
+  grid-template: repeat(1, 1fr) / repeat(4, 1fr);
+  backdrop-filter: blur(10px);
+  padding: 50px 0;
+  border-radius: 50px;
   margin: auto;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  & > *:not(:first-child) {
+    border-left: 1px solid #ffffff;
+  }
+  @media only screen and (max-width: 900px) {
+    grid-template: repeat(2, 1fr) / repeat(2, 1fr);
+  }
+  @media only screen and (max-width: 550px) {
+    grid-template: repeat(4, 1fr) / repeat(1, 1fr);
+  }
 `;
 
 const TokenItem = styled.div`
   display: flex;
+  flex-direction: row;
   align-items: center;
-  margin: 60px 60px;
+  padding: 0 0 0 10%;
 `;
 
-const TokenItemImg = styled.img`
-  background-image: url(${(props) => props.src};);
-  margin-right: 25px;
-`;
-
-const TokenItemText = styled.div`
-  font-family: Gilroy-Medium;
-  font-size: 25px;
-  line-height: 156.8%;
-  color: #ffffff;
-`;
-
-const TokenItemSplit = styled.div`
-  background-image: url(${split});
-  width: 1px;
-  height: 150px;
-`;
+const TokenItemImg = styled.img``;
 
 const Tokenomics = () => {
   return (
@@ -88,49 +86,52 @@ const Tokenomics = () => {
         <TokenomicsText>Tokenomics</TokenomicsText>
         <img
           src={car1}
-          style={{ position: "absolute", top: -331, right: -100, zIndex: -1 }}
+          style={{ position: "absolute", top: -110, right: 200, zIndex: -1 }}
+          width="600px"
         />
-        <TokenItems>
-          <TokenItem>
-            <TokenItemImg src={mmp} />
-            <TokenItemText>
-              MMP Rewards
-              <br />
-              5%
-            </TokenItemText>
-          </TokenItem>
-          <TokenItemSplit />
-          <TokenItem>
-            <TokenItemImg src={receipt} />
-            <TokenItemText>
-              Liquidity Tax
-              <br />
-              2%
-            </TokenItemText>
-          </TokenItem>
-          <TokenItemSplit />
-          <TokenItem>
-            <TokenItemImg src={buyback} />
-            <TokenItemText>
-              BuybackTax
-              <br />
-              3%
-            </TokenItemText>
-          </TokenItem>
-          <TokenItemSplit />
-          <TokenItem>
-            <TokenItemImg src={cardPos} />
-            <TokenItemText>
-              Sell Tax
-              <br />
-              0%
-            </TokenItemText>
-          </TokenItem>
-        </TokenItems>
         <img
           src={car2}
-          style={{ position: "absolute", top: 250, left: 100, zIndex: -1 }}
+          style={{ position: "absolute", bottom: 120, left: 150, zIndex: -1 }}
+          width="450px"
         />
+        <Col align="center">
+          <StyledToken>
+            <TokenItems>
+              <TokenItem>
+                <TokenItemImg src={mmp} />
+                <Col>
+                  <Text>MMP Rewards</Text>
+                  <Text>5% </Text>
+                </Col>
+              </TokenItem>
+
+              <TokenItem>
+                <TokenItemImg src={receipt} />
+                <Col>
+                  <Text>Liquidity Tax</Text>
+                  <Text>2%</Text>
+                </Col>
+              </TokenItem>
+
+              <TokenItem>
+                <TokenItemImg src={buyback} />
+                <Col>
+                  <Text>BuybackTax</Text>
+                  <Text>3%</Text>
+                </Col>
+              </TokenItem>
+
+              <TokenItem>
+                <TokenItemImg src={cardPos} />
+                <Col>
+                  <Text>Sell Tax</Text>
+                  <Text>0%</Text>
+                </Col>
+              </TokenItem>
+            </TokenItems>
+          </StyledToken>
+        </Col>
+
         <RoadmapText>Roadmap</RoadmapText>
       </TokenomicsDiv>
     </Col>
